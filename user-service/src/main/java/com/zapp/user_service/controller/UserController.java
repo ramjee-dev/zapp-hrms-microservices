1914a80962f356f6169e433fb1874ac7228ab4c0
Ramjee Ambedkar <ramjeedev.tech@gmail.com>
2025-06-16 00:36:17 +0530

    usercontroller create api started working on it

--- user-service/src/main/java/com/zapp/user_service/controller/UserController.java
+++ user-service/src/main/java/com/zapp/user_service/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.zapp.user_service.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    public void createUser(){
+
+    }
 }
