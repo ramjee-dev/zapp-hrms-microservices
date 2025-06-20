14135a0c3b8e0a4668ca4ce19838b0a21ab26f57
Ramjee Ambedkar <ramjeedev.tech@gmail.com>
2025-06-20 23:25:39 +0530

    feat: Add gateway with custom routing for core services

--- gateway-service/src/main/java/com/zapp/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/zapp/gateway_service/GatewayServiceApplication.java
@@ -2,12 +2,40 @@ package com.zapp.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator ZappHrmsRouteConfig(RouteLocatorBuilder builder){
+
+		return builder.routes()
+				.route(p->p
+						.path("/zapphrms/client-service/**")
+						.filters(f->f.rewritePath("/zapphrms/client-service/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CLIENT-SERVICE"))
+				.route(p->p
+						.path("/zapphrms/job-service/**")
+						.filters(f->f.rewritePath("/zapphrms/job-service/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://JOB-SERVICE"))
+				.route(p->p
+						.path("/zapphrms/candidate-service/**")
+						.filters(f->f.rewritePath("/zapphrms/candidate-service/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://CANDIDATE-SERVICE"))
+				.build();
+
 	}
 
 }
